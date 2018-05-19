1076b45ce0c6c1c3d85e4c035f1630a2bbca74a4
root <root@ip-172-31-32-134.us-east-2.compute.internal>
2018-05-19 10:20:31 +0000

    fix

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,14 @@
 class ReservationsController < ApplicationController
   before_action :authenticate_user!
 
+  def preload
+    room = Room.find(params[:room_id])
+    today = Date.today
+    reservations = room.reservations.where("start_date >= ? OR end_date >= ?", today, today)
+
+    render json: reservations
+  end
+
   def create
   	@reservation = current_user.reservations.create(reservation_params)
 
@@ -9,6 +17,7 @@ class ReservationsController < ApplicationController
 
 
   private
+  
     def reservation_params
     	params.require(:reservation).permit(:start_date, :end_date, :price, :total, :room_id)
     	
